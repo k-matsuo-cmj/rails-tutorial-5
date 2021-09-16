ceff98693d2d6aed2ce01ef6da24a6b775902d4a
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-09-16 18:59:45 +0900

    ログイン画面追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン成功→ユーザー情報ページにリダイレクト
+      log_in user
+      redirect_to user
+    else
+      # ログイン失敗→エラーメッセージを表示
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
+end
