4ac22e344e8280b04dcff648af90f4d4a08b0aed
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-09-28 15:58:51 +0900

    アカウント有効化完了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,17 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      # ログイン成功→ユーザー情報ページにリダイレクト
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      if @user.activated?
+        # ログイン成功→ユーザー情報ページにリダイレクト
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        message  = "Account not activated."
+        message += "Check Your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       # ログイン失敗→エラーメッセージを表示
       flash.now[:danger] = 'Invalid email/password combination'
