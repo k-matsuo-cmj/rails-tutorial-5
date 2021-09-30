bb70217e0c5ec3e6274d325619dc875927e82c28
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-09-30 13:13:08 +0900

    マイクロポストを操作する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,15 +56,6 @@ class UsersController < ApplicationController
       )
     end
 
-    # ログイン済みユーザーかどうかを確認する
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
     # 正しいユーザーかどうかを確認する
     def correct_user
       @user = User.find(params[:id])
